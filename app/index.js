1ca2fdd84f405dd24ed0574696e14bd07cb3ff92
growthcode <growthcode@gmail.com>
2017-09-21 08:04:03 -0700

    add react router syncHistoryWithStore

--- app/index.js
+++ app/index.js
@@ -6,14 +6,18 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import { checkIfAuthed } from 'helpers/auth'
 import * as reducers from 'redux/modules'
+import { routerReducer, syncHistoryWithStore } from 'react-router-redux'
+import { hashHistory } from 'react-router'
 
 const store = createStore(
-  combineReducers(reducers),
+  combineReducers({...reducers, routing: routerReducer}),
   compose(
     applyMiddleware(thunk),
     window.devToolsExtension ? window.devToolsExtension() : (f) => f
   )
 )
+
+const history = syncHistoryWithStore(hashHistory, store)
 
 function checkAuth (nextState, replace) {
   if (store.getState().users.isFetching === true) {
@@ -35,7 +39,7 @@ function checkAuth (nextState, replace) {
 
 ReactDOM.render(
   <Provider store={store}>
-    {getRoutes(checkAuth)}
+    {getRoutes(checkAuth, history)}
   </Provider>,
   document.getElementById('app')
 )
